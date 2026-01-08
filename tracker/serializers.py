307954c01d67f415366abac9da7c99dcbb79eb94
Gokul Illendula <gokulillendula@gmail.com>
2026-01-08 13:19:47 +0530

    added userdetail serializer

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,19 +1,34 @@
 from rest_framework import serializers
-from  .models import Earned,Spent,User
+from  .models import Earned,Spent,User,Investments
 
 class EarnedSerializer(serializers.ModelSerializer):
     class Meta:
         model=Earned
         fields=['tid','amt','category','date']
+        read_only_fields=['tid','date']
 class SpentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Spent
         fields=['tid','amt','category','date']
+        read_only_fields=['tid','date']
+class InvestmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Investments
+        fields=['tid','amt','category','date']
+        read_only_fields=['tid','date']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['email','balance']
+
+class UserDetailsSerializer(serializers.ModelSerializer):
+    earned=EarnedSerializer(many=True,read_only=True)
+    spent=SpentSerializer(many=True,read_only=True)
+    investments=InvestmentSerializer(many=True,read_only=True)
+    class Meta:
+        model=User
+        fields=['email','balance','earned','spent','investments']
 class RegisterSerializer(serializers.ModelSerializer):
     password=serializers.CharField(
         write_only=True,
--- tracker/views.py
+++ tracker/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate,login as auth_login
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny,IsAdminUser
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView,ListCreateAPIView
+from rest_framework.generics import ListAPIView,ListCreateAPIView,RetrieveAPIView
 from .serializers import EarnedSerializer,SpentSerializer,UserSerializer
 
 
@@ -73,7 +73,8 @@ def user_login(request):
         "email": user.email
     })
 
-
+class UserSummary(RetrieveAPIView):
+    
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def dashboard(request):
