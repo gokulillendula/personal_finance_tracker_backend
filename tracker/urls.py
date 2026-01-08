8f1ea8b2a6e6becea5c6a1b56caa4cc4aac2ef1e
Gokul Illendula <gokulillendula@gmail.com>
2026-01-08 18:01:34 +0530

    modifed the urls to suit modified views

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path,include
-from .views import greet,user_login,spent,UserList,Earned,UserSummary,AddUser,Users
+from .views import greet,user_login,spent,Earned,UserSummary,Users
 
 
 
 urlpatterns = [
     path('greet/',greet,name='greet'),
     path('users/',Users.as_view(),name='users'),
-    path('register/',AddUser.as_view(),name='register'),
+    path('register/',Users.as_view(),name='register'),
     path('login/',user_login,name='user_login'),
     path('user/dashboard/',UserSummary.as_view(),name='dashboard'),
     path('user/spent/',spent.as_view(),name='addSpent'),
